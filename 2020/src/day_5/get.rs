c803f69a4a5f359c2e3d625a2ca56f835e22d92d
Loc Tran <loc@route42.nl>
2020-12-06 17:55:01 +0100

    clean up

--- 2020/src/day_5/get.rs
+++ 2020/src/day_5/get.rs
@@ -9,67 +9,58 @@ pub fn get() {
         let row = &line[..7];
         let columns = &line[7..];
 
-        let mut upper = 127;
-        let mut lower = 0;
-
-        let mut last_char = ' ';
-        for row_char in row.chars() {
-            let lower_upper = get_half(row_char, lower, upper);
-            lower = lower_upper.0;
-            upper = lower_upper.1;
-            last_char = row_char;
-        }
-        let row_value = get_last(last_char, lower, upper);
-
-        upper = 7;
-        lower = 0;
-        last_char = ' ';
-        
-        for columns_char in columns.chars() {
-            let lower_upper = get_half(columns_char, lower, upper);
-            lower = lower_upper.0;
-            upper = lower_upper.1;
-            last_char = columns_char;
-        }
-
-        let columns_value = get_last(last_char, lower, upper);
-        let seat_id = row_value * 8 + columns_value;
+        let row_value = divide(row, 127) * 8;
+        let col_value = divide(columns, 7);
+        let seat_id = row_value + col_value;
         if seat_id > highest {
             highest = seat_id;
         }
         seat_ids.push(seat_id);
     }
+    seat_ids.sort();
     println!("highest {}", highest);
+    println!("my seat {}", get_seat(seat_ids));
+}
+
+fn divide(input: &str, max: i32) -> i32 {
+    let mut lower = 0;
+    let mut upper = max;
+    let mut last_char = ' ';
+    for mutator in input.chars() {
+        let lower_upper = get_half(mutator, lower, upper);
+        lower = lower_upper.0;
+        upper = lower_upper.1;
+        last_char = mutator;
+    }
+    let row_value = get_last(last_char, lower, upper);
+    return row_value;
+}
+
+fn get_seat(seat_ids: Vec<i32>) -> i32 {
     let mut start = 0;
-    seat_ids.sort_by(|a, b| a.partial_cmp(b).unwrap());
     for seat_id in seat_ids {
         if start == 0 {
             start = seat_id;
             continue;
         }
         if seat_id != start + 1 {
-            println!("my seat {}", start + 1);
-            break;
+            return start + 1;
         }
         start = seat_id;
     }
+    return 0;
 }
 
 fn get_last(divider: char, lower: i32, upper: i32) -> i32 {
     if divider == 'F' || divider == 'L' {
         return lower;
     }
-    if divider == 'B' || divider == 'R' {
-        return upper;
-    }
-    return 0;
+    return upper;
 }
+
 fn get_half(divider: char, lower: i32, upper: i32) -> (i32, i32) {
     if divider == 'F' || divider == 'L' {
         return (lower, ((upper + lower + 1) / 2) - 1);
     }
-    if divider == 'B' || divider == 'R' {
-        return (((upper + lower + 1) / 2), upper) ;
-    }
-    return (0, 0);
+    return (((upper + lower + 1) / 2), upper) ;
 }
