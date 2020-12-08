9b3e37b6ec3ab3d48576997fbfff89d9f2c57fe0
Loc Tran <loc@route42.nl>
2020-12-08 18:33:44 +0100

    clean up

--- 2020/src/day_8/get.rs
+++ 2020/src/day_8/get.rs
@@ -11,6 +11,12 @@ pub fn get() {
     }
 }
 
+#[derive(PartialEq)]
+enum ExitCode {
+    END, // end of script
+    SAME, // same instruction again
+}
+
 fn part_1(input: &str) {
     let mut ops: Vec<(String, i64)> = vec![];
     for line in input.trim().lines() {
@@ -20,18 +26,8 @@ fn part_1(input: &str) {
         let value: i64 = value_str.parse().expect("not ok");
         ops.push((op.to_string(), value));
     }
-    let mut acc: i64 = 0;
-    let mut pos: usize = 0;
-    let mut visited: Vec<usize> = vec![];
-    
-    while !visited.contains(&pos) {
-        visited.push(pos);
-        let (op, value) = &ops[pos];
-        let res = execute(op.as_str(), *value, pos, acc);
-        acc = res.0;
-        pos = res.1;
-    }    
-    println!("part 1: {}", acc);
+    let (_, value) = runner(&ops);
+    println!("part 1: {}", value);
 }
 
 fn part_2(input: &str) {
@@ -44,35 +40,40 @@ fn part_2(input: &str) {
         ops.push((op.to_string(), value));
     }
     
-    let mut found = false;
     let mut repl = 0; // replace the 0th element which is nop or jmp
-    while !found {
+    loop {
         let replaced = replace(&ops, repl);
-        let mut acc: i64 = 0;
-        let mut pos: usize = 0;
-        let mut visited: Vec<usize> = vec![];
-        while !visited.contains(&pos) {
-            visited.push(pos);
-            let (op, value) = &replaced[pos];
-            let res = execute(op.as_str(), *value, pos, acc);
-            acc = res.0;
-            pos = res.1;
-            if pos == ops.len() {
-                found = true;
-                break;
-            }
-        }
-    
-        if found {
-            println!("part 2: {}", acc);
-            break;
-        } else {
+        let (exit, acc) = runner(&replaced);
+        if exit == ExitCode::SAME {
             repl += 1;
-            if repl == ops.len() as i32 {
-                println!("not found {}", repl);
-                break
-            }
+            continue;
         }
+        if repl == ops.len() as i32 {
+            println!("part 2: not found");
+            break;
+        }
+        println!("part 2: {}", acc);
+        break
+    }
+}
+
+fn runner(ops: &Vec<(String, i64)>) -> (ExitCode, i64) {
+    let mut acc: i64 = 0;
+    let mut pos: usize = 0;
+    let mut visited: Vec<usize> = vec![];
+
+    loop {
+        let (op, value) = &ops[pos];
+        let res = execute(op.as_str(), *value, pos, acc);
+        acc = res.0;
+        pos = res.1;
+        if pos == ops.len() {
+            return (ExitCode::END, acc);
+        }
+        if visited.contains(&pos) {
+            return (ExitCode::SAME, acc);
+        }
+        visited.push(pos);
     }
 }
 
